6aa073079306c6c8f2fd51dd0a6eb7bc45a841fa
Juno <38800787+hazy-dreams@users.noreply.github.com>
2026-08-19 05:54:04 +0000

    build: upgrade Markdown renderer to 0.43.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.11.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.11.0")
     implementation("com.squareup.okhttp3:okhttp")
-    implementation("com.mikepenz:multiplatform-markdown-renderer-m3:0.39.0")
+    implementation("com.mikepenz:multiplatform-markdown-renderer-m3:0.43.0")
     implementation("org.jetbrains:markdown:0.7.5")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
